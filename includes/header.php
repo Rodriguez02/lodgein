14c4e06edd4c47eb07b4e3e61a53f7949e2d0f12
gonzalo grisafi <gonzagrisa@gmail.com>
2019-11-25 21:39:03 -0300

    added logout functionality

--- includes/header.php
+++ includes/header.php
@@ -15,18 +15,22 @@ if (isset($_SESSION['userLoggedIn'])) {
 <head>
     <meta charset="UTF-8">
     
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <script src="assets/js/script.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/profile.css">
+
+    <title>LodgeIn</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="height: 5em;">
         <div class="d-flex flex-grow-1">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="index.php">
                 <img src="./assets/images/icons/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
                 LodgeIn.com
             </a>
@@ -50,7 +54,15 @@ if (isset($_SESSION['userLoggedIn'])) {
                     <a href="#" class="nav-link">Favoritos</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">Perfil</a>
+                    <a href="settings.php" class="nav-link">Perfil</a>
+                </li>
+                <li>
+                <i class="fas fa-map-marker-alt"></i>
+
+
+                </li>
+                <li class="nav-item">
+                    <a href="includes/logout.php" class="nav-link">Cerrar Sesión</a>
                 </li>
             </ul>
         </div>
--- index.php
+++ index.php
@@ -1,30 +1,58 @@
 <?php include("includes/header.php"); ?>
 
 
-<div class="bg-secondary">
-    <h1 class="text-center text-primary">You might also like</h1>
+<div class="bg-white">
+    <h1 class="text-center text-dark" style="margin-top: 30px;font-weight: 700;">Recomendaciones</h1>
 
     <div class="container">
         <div class='row hidden-md-up'>
-        <?php
-        $albumQuery = mysqli_query($con, "select * from lodgings order by rand()");
+            <?php
+            $lodgeQuery = mysqli_query($con, "SELECT 
+                                                l.id as id,
+                                                l.title as title,
+            	                                l.photo as photo,
+                                                lt.name as type,
+                                                c.name as city,
+                                                s.name as state,
+                                                ct.name as country,
+                                                u.email as email,
+                                                u.phone as phone
+                                            FROM lodgings l
+                                            JOIN lodgings_types lt
+                                                on l.type=lt.id
+                                            JOIN cities c
+                                                on l.city = c.id
+                                            JOIN states s
+                                                on c.state_id=s.id
+                                            JOIN countries ct
+                                                on s.country_id=ct.id
+                                            JOIN users u
+    	                                        on l.publisher=u.id
+                                                order by rand() LIMIT 9");
 
-        while ($row = mysqli_fetch_array($albumQuery)) {
-            echo "
+            while ($row = mysqli_fetch_array($lodgeQuery)) {
+                echo "
                 <div class='col-md-4 py-2'>
-                    <div class='card' style='width: 18rem;
-                                             border: none;'>
+                    <div class='card shadow' style='width: 18rem;
+                                                    border: none;'>
                         <img src='" . $row['photo'] . "' class='card-img-top'>
                         <div class='card-body' style='text-align:center;'>
-                            <h5 class='card-title text-primary'>". $row['title'] . "</h5>
-                            <p class='card-text text-primary'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 class='card-title text-dark' style='font-weight:bolder'>" . $row['title'] . "</h5>
+                            <p class='card-text text-dark'>
+                                <i class='fa fa-home' style='color:black; font-size: 1.5em;'></i> ".$row['type']." <br/><br/>
+                                <i class='fa fa-globe' style='color:black; font-size: 1.5em;'></i>
+                                ".$row['city'].",".$row['state'].",".$row['country']." <br/> <br/>
+                                <i class='fa fa-envelope' style='color:black; font-size: 1em;'></i>  ".$row['email']." <br/>
+                                <i class='fa fa-phone' style='color:black; font-size: 1.2em;'></i>  ".$row['phone']." <br/>
+                            </p>
                             <a href='lodge.php?id=" . $row['id'] . "' class='btn btn-primary'>Información</a>
                         </div>
                     </div>
                 </div>
             ";
-        }
-        ?>
-    </div>
+            }
+            ?>
+        </div>
+
     </div>
 </div>
