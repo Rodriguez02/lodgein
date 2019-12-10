82bb4e02bcab63681651a0a14a1b82e11050efc9
gonzalo grisafi <gonzagrisa@gmail.com>
2019-12-09 22:31:50 -0300

    publications page completed, need to change some css

--- publications.php
+++ publications.php
@@ -1,9 +1,22 @@
-<?php include ("includes/header.php");?>
+<?php include("includes/header.php"); ?>
 
 <?php
 $idUser = $_SESSION['idUser'];
 
-$query = mysqli_query($con,("SELECT * FROM lodgings l WHERE l.publisher = '$idUser'"));
+$query = mysqli_query($con, ("SELECT * FROM lodgings l WHERE l.publisher = '$idUser'"));
+
+$query = mysqli_query($con,(
+    "SELECT l.id as id, l.title as title, l.photo as photo, t.name as type, c.name as city, s.name as state, ct.name as country
+        FROM lodgings l
+        JOIN lodgings_types t
+            on l.type = t.id
+        JOIN cities c
+            on l.city = c.id
+        JOIN states s
+            on c.state_id = s.id
+        JOIN countries ct
+            on s.country_id = ct.id
+        WHERE l.publisher = '$idUser'"));
 
 /* if (mysqli_num_rows($query) == 0) {
     echo "No tienes publiciones activas";
@@ -19,20 +32,43 @@ $query = mysqli_query($con,("SELECT * FROM lodgings l WHERE l.publisher = '$idUs
 
 <div class="mx-auto">
     <h1 class="text-center mt-2" style="font-weight:600">Publicaciones</h1>
-
-    <h2>
+    
+    <div>
         <?php
-            if (mysqli_num_rows($query) == 0){
-                echo "<h1>No tienes publiciones activas</h1>";
+        if (mysqli_num_rows($query) == 0) {
+            echo "<h1 class='text-center mt-5'>No tienes publiciones activas</h1>";
+        } else {
+            while ($row = mysqli_fetch_array($query)) {
+                echo "
+                <div class='card mb-3 shadow' style='max-width: 800px; margin-left: 2em;'>
+                  <div class='row no-gutters'>
+                    <div class='col-md-5'>
+                      <img src='" . $row['photo'] . "' class='card-img'>
+                    </div>
+                    <div class='col-md-7'>
+                      <div class='card-body'>
+                        <h5 class='card-title' style='font-weight:bold;'>" . $row['title'] . "</h5>
+                        <p class='card-text'>
+                        <i class='fa fa-home' style='color:gray; font-size: 1.2em; margin-right:0.5em;'></i> ".$row['type']." <br/>
+                        <i class='fa fa-globe' style='color:gray; font-size: 1.2em; margin-right:0.5em;'></i>
+                        " . $row['city'] . ", " . $row['state'] . ", " . $row['country'] ."
+                        </p>
+                        <div class='row'>
+                            <a href='lodge.php?id=" . $row['id'] . "' class='btn btn-primary mx-auto'
+                                style='font-weight:bold;
+                                border-radius:2em;'
+                            >Información</a>
+                            <button class='delbtn mx-auto' style='width:7em;' onclick='deletePublication(". $row['id'] . ")'>Eliminar</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            ";
             }
-            else{
-                while ($row = mysqli_fetch_array($query)) {
-                    echo "<tr>";
-                    echo "<td>" . $row['title'] . "</td>";
-                    echo "</tr>";
-                }
-            }
+        }
         ?>
-    </h2>
+    </div>
+
 
 </div>
