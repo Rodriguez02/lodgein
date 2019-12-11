0a007109c930a0d3f80e1b504cd805375948dc0c
gonzalo grisafi <gonzagrisa@gmail.com>
2019-12-11 15:52:53 -0300

    added notifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 /* function openPage(url) {
 
 	if(url.indexOf("?") == -1) {
@@ -9,19 +11,35 @@
     history.pushState(null, null, url);
 }
  */
-function updateEmail(emailClass){
+function updateEmail(emailClass) {
     var emailValue = $("." + emailClass).val();
 
     emailValue = emailValue.toLowerCase();
 
-    $.post("includes/handlers/updateEmail.php",{email: emailValue})
-    .done(function(response){
-        console.log(response);
-        $(".txtb" ).nextAll(".m1").text(response);
-    })
+    $.post("includes/handlers/updateEmail.php", { email: emailValue })
+        .done(function (response) {
+            console.log(response);
+            if (response == "Actualización Exitosa") {
+                swal.fire({
+                    icon: 'success',
+                    title: 'Actualización exitosa',
+                })
+            } else if (response == "Email inválido" || response == "Email ya en uso"){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: response
+                })
+            } else{
+                Swal.fire({
+                    icon: 'warning',
+                    title: response,
+                })
+            }
+        })
 }
 
-function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2){
+function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
     var oldPassword = $("." + oldPasswordClass).val();
     var newPassword1 = $("." + newPasswordClass1).val();
     var newPassword2 = $("." + newPasswordClass2).val();
@@ -32,32 +50,65 @@ function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2){
             newPassword1: newPassword1,
             newPassword2: newPassword2
         })
-    .done(function(response){
-        console.log(response);
-        $(".txtb" ).nextAll(".m2").text(response);
-    })
-}
-
-function updatePhone (phoneClass){
-    var newPhone = $("."+phoneClass).val();
-    console.log(newPhone);
-    $.post("includes/handlers/updatePhone.php",{phone:newPhone})
-    .done(function(response){
-        console.log(response);
-        $(".txtb").nextAll(".m3").text(response);
-    })
-}
-
-function deletePublication(id){
-    var prompt = confirm("¿Estás seguro que quieres eliminar esta publicación?");
-    if (prompt){
-        $.post("includes/handlers/deletePublication.php",{id:id})
-        .done(function(error){
-            if (error != ""){
-                alert(error);
-                return;
-            }
-            location.reload();
+        .done(function (response) {
+            console.log(response);
+            $(".txtb").nextAll(".m2").text(response);
         })
-    }
+}
+
+function updatePhone(phoneClass) {
+    var newPhone = $("." + phoneClass).val();
+    console.log(newPhone);
+    $.post("includes/handlers/updatePhone.php", { phone: newPhone })
+        .done(function (response) {
+            console.log(response);
+            if (response == "Teléfono Inválido"){
+                Swal.fire({
+                    icon: 'error',
+                    title: response,
+                })
+            } else if (response == "Debe ingesar un número de teléfono"){
+                Swal.fire({
+                    icon: 'warning',
+                    title: response,
+                })
+            } else{
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Actualización exitosa',
+                    text: response
+                })
+            }
+        })
+}
+
+function deletePublication(id) {
+    swal.fire({
+        title: 'Estás seguro?',
+        text: "Este cambio no se puede revertir!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.value) {
+            $.post("includes/handlers/deletePublication.php", { id: id })
+                .done(function (error) {
+                    if (error != "") {
+                        alert(error);
+                        return;
+                    }
+                })
+            swal.fire(
+                'Eliminado!',
+                'Tu publicación ha sido eliminada con éxito',
+                'success'
+            ).then(() => {
+                location.reload();
+            })
+
+        }
+    })
 }
