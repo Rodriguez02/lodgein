b30b542d6ce57b6e5e47cddf5c61a4a1d4331b1a
José <rodriguezjosee8@gmail.com>
2019-12-15 17:35:03 -0300

    update city selection

--- includes/handlers/actionCities.php
+++ includes/handlers/actionCities.php
@@ -7,13 +7,19 @@ if (isset($_POST['search'])) {
 
     $resp = $_POST['search'];
     $arrayData = array();
-    $sql = "SELECT * from cities WHERE name LIKE '" . $resp . "%'";
+    $sql = "SELECT cities.id, cities.name AS name_city, states.name AS name_state, countries.name AS name_country
+                FROM cities 
+                    JOIN states 
+                    ON cities.state_id = states.id 
+                    JOIN countries 
+                    ON states.country_id = countries.id 
+                WHERE cities.name LIKE '" . $resp . "%' LIMIT 40";
+
     $res = $con->query($sql);
     if ($res->num_rows > 0) {
         while ($row = $res->fetch_array()) {
-            $arrayData[] = array("value"=>$row['id'], "label"=>$row['name']);
+            $arrayData[] = array("value"=>$row['id'], "label"=>($row['name_city'] . ", " . $row['name_state'] . ", " . $row['name_country']));
         }
-        $arrayData = array_slice($arrayData,0, 7);
 
         echo json_encode($arrayData);
     }
