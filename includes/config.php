615bac84af98e4f041e77f87e15a1bee2256afc0
gonzalo grisafi <gonzagrisa@gmail.com>
2019-12-16 15:04:42 -0300

    changed database info

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
     $timeZone = date_default_timezone_set("America/Argentina/Cordoba");
 
-    $con = mysqli_connect("localhost","root","","lodgein");
+    $con = mysqli_connect("localhost","g01devel_root","panchopereyra07","g01devel_lodgein","3306");
 
     if (mysqli_connect_errno()){
         echo "Failed to connect: " . mysqli_connect_errno();
